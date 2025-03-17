11e28507852eb9bb7631b1abcefefbeaa2d1a979
junhyeonglee <junhyeong@snaptag.co.kr>
2025-03-17 14:24:06 +0900

    로깅 함수 예외 처리 추가

--- lib/features/core/printer/printer_log.dart
+++ lib/features/core/printer/printer_log.dart
@@ -4,15 +4,11 @@ import 'package:flutter_snaptag_kiosk/features/core/printer/ribbon_status.dart';
 class PrinterLog {
   PrinterStatus? printerStatus;
   RibbonStatus? ribbonStatus;
-  bool isPrintingNow;
-  bool isFeederEmpty;
+  bool? isPrintingNow;
+  bool? isFeederEmpty;
   String? errorMsg;
 
-  PrinterLog({
-    required this.printerStatus,
-    required this.ribbonStatus,
-    required this.isPrintingNow,
-    required this.isFeederEmpty,
-    this.errorMsg,
-  });
+  PrinterLog.init();
+
+  PrinterLog(this.printerStatus, this.ribbonStatus, this.isFeederEmpty, this.isPrintingNow, this.errorMsg);
 }
