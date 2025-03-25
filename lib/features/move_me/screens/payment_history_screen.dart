fa9c035d72f4d986fc85cea8d694f4d8c1f3a317
yannSnaptag <yann@snaptag.co.kr>
2025-03-25 14:46:05 +0900

    feat: 결제 중 ui-block overlay 도입
    why:
    결제 환불 중 사용자가 '환불하기' 버튼을 두 번 누르며 발생할 수 있는 중복 요청을 방지하고자 함
    
    what:
    - 환불 프로세스 중 UI 상호작용 차단 오버레이 추가
    - 로딩 중임을 알리는 인디케이터 화면 중앙에 노출
    
    how:
    - loader_overlay 패키지를 활용해 환불 버튼 클릭 시 context.loaderOverlay.show() 호출
    - 환불 완료 시 ref.listen() 내부에서 context.loaderOverlay.hide() 처리
    
    tags: #ui, #overlay, #loading

--- lib/features/move_me/screens/payment_history_screen.dart
+++ lib/features/move_me/screens/payment_history_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_snaptag_kiosk/core/utils/sound_manager.dart';
 import 'package:flutter_snaptag_kiosk/lib.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class PaymentHistoryScreen extends ConsumerStatefulWidget {
   const PaymentHistoryScreen({super.key});
@@ -17,6 +18,11 @@ class _PaymentHistoryScreenState extends ConsumerState<PaymentHistoryScreen> {
   @override
   Widget build(BuildContext context) {
     ref.listen(setupRefundProcessProvider, (prev, next) {
+
+      if (context.loaderOverlay.visible) {
+        context.loaderOverlay.hide();
+      }
+
       next.whenOrNull(
         error: (error, stack) async {
           await DialogHelper.showRefundFailDialog(context);
@@ -299,6 +305,7 @@ class _PaymentHistoryScreenState extends ConsumerState<PaymentHistoryScreen> {
                   confirmButtonText: '환불 진행',
                 );
                 if (result2) {
+                  context.loaderOverlay.show();
                   await ref.read(setupRefundProcessProvider.notifier).startRefund(order);
                 }
               },
@@ -323,8 +330,6 @@ class _PaymentHistoryScreenState extends ConsumerState<PaymentHistoryScreen> {
               ),
               onPressed: () async {
                 await SoundManager().playSound();
-
-                ;
                 final result1 = await DialogHelper.showSetupDialog(
                   context,
                   title: '환불을 진행합니다.',
