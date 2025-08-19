cdbd2769fdeeaf2328cee1c59c167163723b25d5
yannSnaptag <yann@snaptag.co.kr>
2025-08-19 15:28:19 +0900

    chore: 버전 정보 변경(2.8.0->2.9.0)

--- lib/data/repositories/version_repository.dart
+++ lib/data/repositories/version_repository.dart
@@ -31,7 +31,7 @@ class VersionRepository {
   Future<String> getCurrentVersion() async {
     final envFile = File('assets/.env.version');
     final version = await envFile.readAsString();
-    return version ?? 'v2.8.0';
+    return version ?? 'v2.9.0';
   }
 
   Future<String> getLatestVersionFromGitHub() async {
