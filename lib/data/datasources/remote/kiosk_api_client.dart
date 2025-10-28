c9a27f7b2cfa31ef3ede8e23fc2f8916ce18d475
junhyeongleeeee <junhyeong@snaptag.co.kr>
2025-10-28 15:49:55 +0900

    feat: - checkKioskAlive URL 수정

--- lib/data/datasources/remote/kiosk_api_client.dart
+++ lib/data/datasources/remote/kiosk_api_client.dart
@@ -83,7 +83,7 @@ abstract class KioskApiClient {
       @Path('machineId') required int machineId,
       @Path('remainingSingleSidedCount') required int remainingSingleSidedCount});
 
-  @POST('event/check/{kioskEventId}/{machineId}/{remainingSingleSidedCount|}')
+  @POST('/v1/internal/event/check/{kioskEventId}/{machineId}/{remainingSingleSidedCount}')
   Future<void> checkKioskAlive(
       {@Path('kioskEventId') required int kioskEventId,
       @Path('machineId') required int machineId,
