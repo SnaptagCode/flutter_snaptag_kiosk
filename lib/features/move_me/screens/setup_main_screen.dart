970d0c0bdd98b425ecb8496d254d6433b4439e96
junhyeongleeeee <junhyeongleeeee@snaptag.co.kr>
2025-12-23 16:18:02 +0900

    feat: - v3.3.0

--- lib/features/move_me/screens/setup_main_screen.dart
+++ lib/features/move_me/screens/setup_main_screen.dart
@@ -549,7 +549,7 @@ class _SetupMainScreenState extends ConsumerState<SetupMainScreen> {
                       child: SetupUpdateCard(
                         title: '현재 버전',
                         //version: currentVersion,
-                        version: "v3.2.2",
+                        version: "v3.3.0",
                         buttonName: '업데이트',
                         isActive: isUpdateAvailable,
                         onUpdatePressed: () async {
